03339c37841cf9d8cd5037669486061f60670a81
filiptota <filip.tota5@gmail.com>
2025-08-08 12:33:21 +0200

    project setup, introducing middleware

---
+++ src/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getEmoji(): string {
+    const emojis = this.getEmojis();
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    return emojis[randomIndex];
+  }
+
+  getEmojis() {
+    return [
+      '👋',
+      '👍',
+      '👎',
+      '👌',
+      '👊',
+      '👏',
+      '👐',
+      '👑',
+      '👣',
+      '👤',
+      '👥',
+      '👐',
+      '👑',
+    ];
+  }
+}
