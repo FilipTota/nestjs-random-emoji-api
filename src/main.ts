2d490fa25312eb46e96ec58d10491b9fe23678ab
filiptota <filip.tota5@gmail.com>
2025-08-13 12:36:00 +0200

    starting deployment again after railway login command

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for Railway deployment
+  // CORS for Railway deployment
   app.enableCors({
     origin: true,
     credentials: true,
